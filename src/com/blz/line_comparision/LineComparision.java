e02637051d6ab5355d230bb772a53a287c90d12f
akashsathe063 <akashsathe063@gmail.com>
2022-07-10 19:59:48 +0530

    Well come to line comparision computation

---
+++ src/com/blz/line_comparision/LineComparision.java
@@ -0,0 +1,7 @@
+package com.blz.line_comparision;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparision computation");
+    }
+}
